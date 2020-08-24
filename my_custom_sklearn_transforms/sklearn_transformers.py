40071b59b00184d3bbe80bdd2927c9f31d02d967
PROFESSOR ROGERIO B. ANDRADE <31871144+profrbandrade@users.noreply.github.com>
2020-08-24 10:23:24 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,11 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin, preprocessing
 
 
-def scaleColumns(df, cols_to_scale):
-    for col in cols_to_scale:
-        df[col] = pd.DataFrame(min_max_scaler.fit_transform(pd.DataFrame(df_data_1[col])),columns=[col])
-    return df
-
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -19,3 +14,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    def scaleColumns(df, cols_to_scale):
+    for col in cols_to_scale:
+        df[col] = pd.DataFrame(min_max_scaler.fit_transform(pd.DataFrame(df_data_1[col])),columns=[col])
+    return df
