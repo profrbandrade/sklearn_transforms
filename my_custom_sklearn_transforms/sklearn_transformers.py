b715acc15bc2ae76d3a742975c9c420f98f0e129
PROFESSOR ROGERIO B. ANDRADE <31871144+profrbandrade@users.noreply.github.com>
2020-08-24 10:42:15 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,5 +14,9 @@ class DropColumns(BaseEstimator, TransformerMixin,MinMaxScaler):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    def scaleColumns(df, cols_to_scale):
+        for col in cols_to_scale:
+            df[col] = pd.DataFrame(min_max_scaler.fit_transform(pd.DataFrame(df_data_1[col])),columns=[col])
+        return df
     
   
